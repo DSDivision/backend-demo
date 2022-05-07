c763f72b919db8cc7d8e1fc345d5593054d0ae74
siimkas <kasela.siim@gmail.com>
2022-05-07 19:39:13 +0300

    Basic search for test kit
    Added some basic search functionality to find movier or series from .csv file

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
 import pandas as pd
 
 app = FastAPI()
 
-data = pd.read_csv("data/netflix_titles.csv")
+data = pd.read_csv("data/netflix_titles.csv",index_col=0).fillna("null")
 
 @app.get("/recommendation")
-async def recommendation():
-    sample = data.sample().iloc[0]
-    return {"movie": {
-            "title": sample.title,
-            "genre": sample.listed_in,
-            "description": sample.description
-        }
-    }
+async def recommendation(limit: int | None = Query(10, description="Max nr of movies to list."),
+                         title: str | None = Query("", description="Search by title"),
+                         genre: str | None = Query("", description="Search by genre"),
+                         director: str | None = Query("", description="Search by director"),
+                         actor: str | None = Query("", description="Search by actor"),
+                         keyword: str | None = Query("", description="Search by a keyword from description"),
+                         release_year: int | None = Query(None, description="Search by release year")):
+
+    
+    sample = data.loc[data['title'].str.contains(title) &
+                      data['listed_in'].str.contains(genre) &
+                      data['director'].str.contains(director) &
+                      data['cast'].str.contains(actor) &
+                      data['description'].str.contains(keyword)]
+    if release_year!=None:
+        sample = sample.loc[sample['release_year']==release_year]
+
+    return sample.head(limit).to_dict(orient="index")
+
+@app.get("/recommendation/{show_id}")
+async def getMovie(show_id: str = Path(None, description="An id of the show")):
+    return data.loc[show_id].to_dict()
