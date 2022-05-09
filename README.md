e68c9087305a08e2d07d3869b510cf7bb26df295
siimkas <kasela.siim@gmail.com>
2022-05-09 12:35:21 +0300

    Added 'Not Found' Exceptions

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ uvicorn main:app --reload
 Access the api in your browser, at: http://127.0.0.1:8000/docs.
 There you can see the generated docs for the api, and can try out the functionality.
 Currently there are 2 GET functions, one with basic search functionality and other for getting a specific movie by id.
-No errors are caught atm.
+Raises 404 Exception when nothing is found.
 
 
 Have fun :)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Path, Query
+from telnetlib import STATUS
+from fastapi import FastAPI, Path, Query, HTTPException, status
 import pandas as pd
 
 app = FastAPI()
@@ -22,9 +23,12 @@ async def recommendation(limit: int | None = Query(10, description="Max nr of mo
                       data['description'].str.contains(keyword)]
     if release_year!=None:
         sample = sample.loc[sample['release_year']==release_year]
-
+    if(len(sample)==0):
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail="No media found")
     return sample.head(limit).to_dict(orient="index")
 
 @app.get("/recommendation/{show_id}")
 async def getMovie(show_id: str = Path(None, description="An id of the show")):
+    if(show_id not in data.index):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Movie with that id does not exist")
     return data.loc[show_id].to_dict()
